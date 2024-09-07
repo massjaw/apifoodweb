9a34ac2bbc00c3fa83adba9b5d256a196b37e8be
massjaw <ibrahimhisyam85@gmail.com>
2024-09-07 15:36:48 +0700

    set viper and logrus, everythings already setup

--- main.go
+++ main.go
@@ -1,9 +1,46 @@
 package main
 
-func init() {
+import (
+	"os"
 
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+)
+
+var Environment string
+
+func init() {
+	initLogrus()
+	initViper()
 }
 
 func main() {
+	logrus.Infoln("application start: this apps work on " + Environment + " environment.")
+}
 
+func initViper() {
+
+	viper.SetConfigName("config")
+	viper.SetConfigType("json")
+	viper.AddConfigPath("config/")
+
+	if errConfig := viper.ReadInConfig(); errConfig != nil {
+		logrus.Panicln("failed to read config:", errConfig)
+	}
+
+	Environment = "\033[31m" + viper.GetString("Environment") + "\033[0m"
+
+}
+
+func initLogrus() {
+
+	logrus.SetOutput(os.Stdout)
+
+	logrus.SetFormatter(&logrus.TextFormatter{
+		ForceColors:     true,
+		FullTimestamp:   true,
+		TimestampFormat: "2006-01-02 15:04:05",
+		DisableSorting:  false,
+		PadLevelText:    true,
+	})
 }
