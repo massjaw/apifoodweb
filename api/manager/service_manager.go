6c190df1b323277fc197a35b78ef3b35ef95dd9d
massjaw <ibrahimhisyam85@gmail.com>
2024-10-20 22:03:14 +0700

    [SERVICE]-[renaming functtion name to make function naming more consistent]

--- api/manager/service_manager.go
+++ api/manager/service_manager.go
@@ -3,14 +3,14 @@ package manager
 import "apifoodweb/api/service"
 
 type ServiceManager interface {
-	UserUsecase() service.UserService
+	UserService() service.UserService
 }
 
 type serviceManager struct {
 	repoManager RepoManager
 }
 
-func (u *serviceManager) UserUsecase() service.UserService {
+func (u *serviceManager) UserService() service.UserService {
 	return service.NewUserService(u.repoManager.UserRepo())
 }
 
